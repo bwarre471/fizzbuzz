aeb76ccef70b49706f135ba8b32566b1a17e80b9
bwArre471 <140698659+bwarre471@users.noreply.github.com>
2023-08-17 20:29:12 +0200

    FizzBuzz
    Defining FizzBuzz without modulo,...

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+let i = 1
+let k = 1
+let t = 1
+while(i<=100) {
+    let display = true
+    if(k==3) {
+        process.stdout.write("Fizz")
+        display = false
+        k = 0
+    }
+    if(t==5) {
+        process.stdout.write("Buzz")
+        display = false
+        t = 0
+    }
+}
